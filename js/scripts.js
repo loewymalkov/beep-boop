d9f210e5aabb75a4f46f496913a8237211d5c596
Loewy Malkovich <loewymalkov@gmail.com>
2019-08-23 10:34:29 -0700

    begin building business logic for 1, 2, 3 rules

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
 // business logic
+var userInputArray = [];
+var outputArray = [];
+// return a range of numbers starting at 0 to the user inputted number
+
+// numbers that contain a 1 --> replace with "Beep!"
+if (userInputArray.includes(1)) {
+  outputArray.push("Beep!");
+  console.log(outputArray); 
+} else {
+  console.log('does not contain 1');
+}
+// numbers that contain a 2 --> replace with "Boop!"
+if (userInputArray == 2) {
+  outputArray.push("Boop!"); 
+};
+// numbers that contain a 3 --> replace with "I'm sorry, " statement
+if (userInputArray == 3) {
+  outputArray.push("I'm sorry, Dave. I'm afraid I can't do that."); 
+};
 
 
 
