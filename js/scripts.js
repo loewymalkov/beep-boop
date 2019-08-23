8b195b768b21062be5192f27e62809bb23952ca5
Loewy Malkovich <loewymalkov@gmail.com>
2019-08-23 14:15:36 -0700

    make changes to structure of loops so it splits array indexes and checks for rules

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,39 @@
 // business logic
 var input = "10";
-var userInputArray = [];
-var outputArray = [];
+var userInputRange = [];
 
 // use spread operator to make a copy of array or add other values 
+
 // return a range of numbers starting at 0 to the user inputted number
 for (var i = 0; i <= input; i++) {
-  userInputArray.push(i); 
-  console.log(userInputArray);
+  userInputRange.push(i); 
+  console.log(userInputRange);
 };
+var outputArray = [...userInputRange];
+console.log(outputArray);
+
+// make a for loop to go through all the elements w/ rules
+
+for (var i=0; i < outputArray.length; i++) {
+  if (outputArray[i].split() === 1 ) {
+    
+  }
+}
 // numbers that contain a 1 --> replace with "Beep!"
-if (userInputArray.includes(1)) {
+if (outputArray.includes(1)) {
   outputArray.splice(index, "Beep!");
   console.log(outputArray); 
 } else {
   console.log('does not contain 1');
 }
 // numbers that contain a 2 --> replace with "Boop!"
-if (userInputArray == 2) {
+if (outputArray == 2) {
   outputArray.push("Boop!"); 
 } else {
 
 };
 // numbers that contain a 3 --> replace with "I'm sorry, " statement
-if (userInputArray == 3) {
+if (outputArray == 3) {
   outputArray.push("I'm sorry, Dave. I'm afraid I can't do that."); 
 } else {
 
