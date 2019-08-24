334fd106e5687428456ad1f2715455ab6c926050
Loewy Malkovich <loewymalkov@gmail.com>
2019-08-24 11:05:36 -0700

    finish branching structure and test replacement rules, back end only needs name input added

--- js/scripts.js
+++ js/scripts.js
@@ -16,32 +16,16 @@ var outputArray = [...userInputRange];
 for (var i=1; i <= outputArray.length; i++) {
   var stringNumber = outputArray[i].toString();
   var splitArray = (stringNumber + '').split();
-    if (splitArray[0].includes("1")) {
+    if (splitArray[0].includes("3")) {
+      outputArray.splice(i, 1, "I'm sorry, name. I'm afraid I can't do that.")
+    } else if (splitArray[0].includes("2")) {
+      outputArray.splice(i, 1, "Boop!")
+    } else if (splitArray[0].includes("1")) {
       outputArray.splice(i, 1, "Beep!")
     } else {
       console.log('nothing to replace');
     };
   console.log(outputArray);
-};
-
-// numbers that contain a 1 --> replace with "Beep!"
-if (outputArray.includes(1)) {
-  outputArray.splice(index, "Beep!");
-  console.log(outputArray); 
-} else {
-  console.log('does not contain 1');
-}
-// numbers that contain a 2 --> replace with "Boop!"
-if (outputArray == 2) {
-  outputArray.push("Boop!"); 
-} else {
-
-};
-// numbers that contain a 3 --> replace with "I'm sorry, " statement
-if (outputArray == 3) {
-  outputArray.push("I'm sorry, Dave. I'm afraid I can't do that."); 
-} else {
-
 };
 
 // reverse operation order so that rule 3 takes presedence over 2&1 and 2 over 1
